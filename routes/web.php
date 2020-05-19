ef7b5bee00671d5939e6815e3bdf0af830242b8e
YooJiseong <jisung87kr@gmail.com>
2020-05-19 21:10:13 +0900

    리다이렉트 수정

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return route()->redirect('posts.index');
+    return redirect()->route('posts.index');
 });
 
 Auth::routes();
